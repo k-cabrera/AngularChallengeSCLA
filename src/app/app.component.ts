33848ae0671ab482cb8d4d0f2bd40134c33004a8
k-cabrera <62921719+k-cabrera@users.noreply.github.com>
2020-07-30 01:08:56 -0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
+  
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators} from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularChallenge';
+  title = 'angular-first-task';
+  public form: FormGroup;
+  public blogs =[];
+
+constructor(public _formBuilder: FormBuilder) {
+  this.form = this._formBuilder.group(
+    {
+      title: ['', Validators.required],
+      content: ['', Validators.required],
+    }
+  );
+}
+
+onSubmit() {
+  console.log(this.form.value);
+  
+  this.blogs.push(this.form.value)
+
+  
+}
+
 }
